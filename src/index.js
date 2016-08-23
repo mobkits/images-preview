8e3f013446f11fdb90c42ccf23687580d1fe9f89
chemzqm <chemzqm@gmail.com>
2016-08-23 17:58:04 +0800

    add holder animation

--- src/index.js
+++ src/index.js
@@ -10,10 +10,8 @@ import PinchZoom from 'pinch-zoom'
 import assign from 'object-assign'
 import spin from './spin'
 import domify from 'domify'
-import detect from 'prop-detect'
+import {has3d, transform} from 'prop-detect'
 
-const has3d = detect.has3d
-const transform = detect.transform
 const doc = document
 
 class ImagesPreview extends Emitter {
@@ -30,17 +28,20 @@ class ImagesPreview extends Emitter {
     let idx = this.imgs.indexOf(el)
     if (idx !== -1) {
       this.show()
-      this.active(el, idx)
+      this.active(el, idx, true)
     }
   }
   show() {
     let div = this.container = document.createElement('div')
     div.id = 'images-preview'
+    setTimeout(() => {
+      div.style.backgroundColor = 'rgba(0,0,0,1)'
+    }, 100)
     let vw = Math.max(doc.documentElement.clientWidth, window.innerWidth || 0)
     div.style.width = (vw*this.imgs.length + 40) + 'px'
     this.setTransform(-20)
     document.body.appendChild(div)
-    let dots = this.dots = domify(`<div class="preview-dots"><ul></ul></div>`)
+    let dots = this.dots = domify(`<div class="imgs-preview-dots"><ul></ul></div>`)
     document.body.appendChild(dots)
     let ul = query('ul', dots)
     let fragment = document.createDocumentFragment()
@@ -56,9 +57,9 @@ class ImagesPreview extends Emitter {
       </div>`))
       let rect = this.imgs[i].getBoundingClientRect()
       assign(wrapper.style, {
-        width: `${rect.width}px`,
-        height: `${rect.height}px`,
-        left: `${(vw - rect.width)/2}px`,
+        width: `${rect.width - 10}px`,
+        height: `${rect.height - (10*rect.height/rect.width)}px`,
+        left: `${(vw - (rect.width - 10))/2}px`,
         marginTop: `-${rect.height/2}px`
       })
       el.appendChild(wrapper)
@@ -67,7 +68,7 @@ class ImagesPreview extends Emitter {
     div.appendChild(fragment)
     this.zooms = []
   }
-  active(img, idx) {
+  active(img, idx, animate = false) {
     let vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
     let state = this.status[idx]
     let wrapper = this.container.querySelectorAll('.wrapper')[idx]
@@ -79,12 +80,29 @@ class ImagesPreview extends Emitter {
       this.status[idx] = 'loading'
       let image = document.createElement('img')
       image.className = 'image'
+      if (animate) {
+        let holder = this.holder = document.createElement('div')
+        holder.className = 'imgs-preview-holder'
+        let src = img.src
+        holder.style.backgroundImage = `url('${src}')`
+        let rect = img.getBoundingClientRect()
+        assign(holder.style, {
+          left: `${rect.left}px`,
+          top: `${rect.top}px`,
+          width: `${rect.width}px`,
+          height: `${rect.height}px`
+        })
+        document.body.appendChild(holder)
+      } else {
+        image.style.display = 'block'
+      }
       let convert = this.opts.convert || function (src) {
         return src
       }
       image.src = convert(img.src)
       wrapper.appendChild(image)
       let pz = new PinchZoom(wrapper, {
+        padding: 5,
         tapreset: true,
         draggable: true,
         maxScale: 4
@@ -100,7 +118,7 @@ class ImagesPreview extends Emitter {
       })
       pz.on('move', dx => {
         let x = - 20 - tx - dx
-        x = Math.min(20, x)
+        x = Math.min(0, x)
         x = Math.max(-40 - (this.imgs.length - 1)*vw, x)
         this.setTransform(x)
         if (dx != 0) pz.speed = 0
@@ -114,50 +132,60 @@ class ImagesPreview extends Emitter {
         })
       })
       this.zooms.push(pz)
+      pz.draggable = false
 
-      this.loadImage(image, wrapper)
+      this.loadImage(image, wrapper).then(() => {
+        pz.draggable = true
+      })
     }
   }
   loadImage(image, wrapper) {
     let vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
     if (image.complete) {
       let dims = imgDimension(image)
-      let h = vw*dims.height/dims.width
+      let h = (vw - 10)*dims.height/dims.width
       let mask = query('.mask', wrapper)
       if (mask) wrapper.removeChild(mask)
       assign(wrapper.style, {
-        left: 0,
-        width: `${vw}px`,
+        left: '5px',
+        width: `${vw - 10}px`,
         height: `${h}px`,
         marginTop: `-${h/2}px`
       })
-      return Promise.resolve(dims)
-    } else {
-      let spinEl = domify(` <div class="spin"></div>`)
-      let mask = query('.mask', wrapper)
-      mask.appendChild(spinEl)
-      let stop = spin(spinEl, {
-        color: '#ffffff',
-        duration: 1000,
-        width: 4
+      return this.positionHolder(wrapper, image.src).then(() => {
+        image.style.display = 'block'
       })
-      return new Promise((resolve, reject) => {
-        image.onload = () => {
-          stop()
-          wrapper.removeChild(mask)
-          let dims = imgDimension(image)
-          let h = vw*dims.height/dims.width
-          assign(wrapper.style, {
-            left: 0,
-            width: `${vw}px`,
-            height: `${h}px`,
-            marginTop: `-${h/2}px`
-          })
-          resolve(dims)
-        }
-        image.onerror = (e) => {
-          reject(e)
-        }
+    } else {
+      return this.positionHolder(wrapper).then(() => {
+        image.style.display = 'block'
+        let mask = query('.mask', wrapper)
+        let spinEl = domify(`<div class="spin"></div>`)
+        mask.appendChild(spinEl)
+        let stop = spin(spinEl, {
+          color: '#ffffff',
+          duration: 1000,
+          width: 4
+        })
+        return new Promise((resolve, reject) => {
+          function onload() {
+            stop()
+            wrapper.removeChild(mask)
+            let dims = imgDimension(image)
+            let h = (vw - 10)*dims.height/dims.width
+            assign(wrapper.style, {
+              left: '5px',
+              width: `${vw - 10}px`,
+              height: `${h}px`,
+              marginTop: `-${h/2}px`
+            })
+            resolve(dims)
+          }
+          if (image.complete) return onload()
+          image.onload = onload
+          image.onerror = (e) => {
+            reject(e)
+          }
+        })
       })
     }
   }
@@ -174,7 +202,7 @@ class ImagesPreview extends Emitter {
     if (x == this.tx) return Promise.resolve(null)
     this.animating = true
     this.zooms.forEach(pz => pz.animating = true)
-    var tween = this.tween = Tween({x: this.tx})
+    let tween = this.tween = Tween({x: this.tx})
       .ease(ease)
       .to({x: x})
       .duration(duration)
@@ -183,11 +211,61 @@ class ImagesPreview extends Emitter {
       self.setTransform(o.x)
     })
     let self = this
-    var promise = new Promise(function (resolve) {
+    let promise = new Promise(function (resolve) {
       tween.on('end', function(){
         self.zooms.forEach(pz => pz.animating = false)
         animate = function(){} // eslint-disable-line
         self.animating = false
+        resolve()
+      })
+    })
+
+    function animate() {
+      raf(animate)
+      tween.update()
+    }
+
+    animate()
+    return promise
+  }
+  positionHolder(wrapper, src) {
+    let el = this.holder
+    if (!el) return Promise.resolve(null)
+    if (src) el.style.backgroundImage = `url('${src}')`
+    let rect = wrapper.getBoundingClientRect()
+    let tween = Tween({
+      width: parseInt(el.style.width),
+      height: parseInt(el.style.height),
+      left: parseInt(el.style.left),
+      top: parseInt(el.style.top),
+      opacity: 0
+    })
+    .ease('linear')
+    .to({
+      width: parseInt(rect.width),
+      height: parseInt(rect.height),
+      left: parseInt(rect.left),
+      top: parseInt(rect.top),
+      opacity: 0.6
+    })
+    .duration(200)
+
+    tween.update(function(o) {
+      assign(el.style, {
+        width: `${o.width}px`,
+        height: `${o.height}px`,
+        left: `${o.left}px`,
+        top: `${o.top}px`,
+        opacity: o.opacity
+      })
+    })
+
+    let self = this
+    let promise = new Promise(function (resolve) {
+      tween.on('end', function(){
+        if (el.parentNode) el.parentNode.removeChild(el)
+        self.holder = null
+        animate = function(){} // eslint-disable-line
         resolve()
       })
     })
